61115653f8b0f1b0633bf8f96f9a5f43247adc61
Mehdi Sebbar <mehdi.sebbar.0@gmail.com>
2016-11-29 17:28:51 +0100

    modifs + notebook

--- weight_vector_estim_code/main.py
+++ weight_vector_estim_code/main.py
@@ -1,11 +1,12 @@
 from densities import generate_sample
 from algorithm import WeightEstimator
 
-X, densities_dict, selected_densities, weights = generate_sample(1000, dim=5, n_densities=10, K=100, var=10**(-3))
+X, densities_dict, selected_densities, weights = generate_sample(1000, dim=5, n_densities=3, K=200, var=10**(-2))
+
 select_weighted_densities = zip(selected_densities, weights)
 select_weighted_densities.sort(key=lambda x: x[0])
 
-cl = WeightEstimator(densities_dict=densities_dict)
+cl = WeightEstimator(densities_dict=densities_dict, select_threshold=10e-4)
 cl.fit(X)
 estim_weighted_densities=cl.select_densities()
 
